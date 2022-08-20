207538ebfc71a18d8263d0984973df2687bf0d93
[Honeybee1009] <jkbest0901@naver.com>
2022-08-20 14:40:33 +0900

    commit message

---
+++ js/letter_mb2.js
@@ -0,0 +1,36 @@
+
+
+let slides1 = document.querySelector('.slides1'),
+slide1 = document.querySelectorAll('.slides1 li'),
+currentIdx1 = 0,
+slideCount1 = slide1.length,
+slideWidth1 = 100,
+slideMargin1 = 30,
+prevBtn1 = document.querySelector('.prev1'),
+nextBtn1 = document.querySelector('.next1');
+
+slides1.style.width = (slideWidth1 + slideMargin1)*slideCount1 - slideMargin1 + 'px';
+
+function moveSlide1(num) {
+    slides1.style.left = -num * 130 + 'px';
+    currentIdx1 = num;
+}
+
+nextBtn1.addEventListener('click', function() {
+
+    if(currentIdx1<slideCount1-3){
+        moveSlide1(currentIdx1 + 1);
+        console.log(currentIdx1);
+    } else{
+        moveSlide1(0);
+    }
+});
+
+prevBtn1.addEventListener('click', function() {
+
+    if(currentIdx1 > 0){
+        moveSlide1(currentIdx1 - 1);
+    } else{
+        moveSlide1(slideCount1 - 3);
+    }
+});
--- js/letter_pc2.js
+++ js/letter_pc2.js
@@ -1,18 +1,18 @@
 
 
-var slides = document.querySelector('.slides1'),
-slide = document.querySelectorAll('.slides1 li'),
+let slides = document.querySelector('.slides'),
+slide = document.querySelectorAll('.slides li'),
 currentIdx = 0,
 slideCount = slide.length,
-slideWidth = 100,
+slideWidth = 300,
 slideMargin = 30,
-prevBtn = document.querySelector('.prev1'),
-nextBtn = document.querySelector('.next1');
+prevBtn = document.querySelector('.prev'),
+nextBtn = document.querySelector('.next');
 
 slides.style.width = (slideWidth + slideMargin)*slideCount - slideMargin + 'px';
 
 function moveSlide(num) {
-    slides.style.left = -num * 130 + 'px';
+    slides.style.left = -num * 330 + 'px';
     currentIdx = num;
 }
 
